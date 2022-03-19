96e248a2c5b92267d0176e3bf55646845dbbdbec
hwbarrows@gmail.com <hwbarrows@gmail.com>
2022-03-19 08:51:43 +0100

    added/ removed event handlers

--- bin.js
+++ bin.js
@@ -3,6 +3,12 @@
 const pop = document.getElementById("popUp")
 pop.className = "popUp"
 
+const imgContainer = document.getElementById("imgContainer")
+const title = document.getElementById("title")
+const text = document.getElementById("text")
+const link = document.getElementById("link")
+const close = document.getElementById("close")
+
 const container = document.querySelector(".flex")
 const filler = Array.from(document.querySelectorAll(".fillers"))
 
@@ -13,57 +19,30 @@ filler.forEach(item => {
 
 })
 
+const closePop = ()=> {
+    container.addEventListener("click", makePopup)
+}
+
 const makePopup = (e) => {
     if (pop.style.opacity == "0" && pop.style.zIndex == "-1" && e.target.id && e.target.id != "popUp") { //replace entire section with class
         pop.style.opacity = "1"
         pop.style.zIndex = "2"
-
-
-        const imgContainer = document.createElement("div")// create class
-        imgContainer.className = "imgContainer"
         imgContainer.style.backgroundImage = `url(./assets/${e.target.id}.jpg)`
-
-        const textContainer = document.createElement("div")
-        textContainer.className = "textContainer"
-
-        const h1 = document.createElement("h1")
-        const p = document.createElement("p")
-        const h4 = document.createElement("h4")
-        const pLink = document.createElement("p")
-        
-
-
 
         arrayOfObjs.filter(item => {
             if (item.name.toLowerCase() == e.target.id){
-                h1.innerHTML = item.name 
-                h4.innerText = `Artwork by ${item.artist}`
-                p.innerHTML = item.summary
-                
-                pLink.innerHTML = `<a href=${item.link} target="_blank">Link to original artwork</a>`
+                title.innerText = item.name
+                text.innerText = `by ${item.artist} \n ${item.summary}`
+                link.innerHTML = `<a href=${item.link} target="_blank">Link to original artwork</a>`
             }
-
         })
-
-        textContainer.appendChild(h1)
-        textContainer.appendChild(h4)
-        textContainer.appendChild(p)
-        
-        textContainer.appendChild(pLink)
-
-        pop.appendChild(imgContainer)
-        pop.appendChild(textContainer)
         container.removeEventListener("click", makePopup)
-
+        
+        close.addEventListener("click", closePop)
 
     }else {
         pop.style.opacity = "0"
         pop.style.zIndex = "-1"
-        while (pop.firstChild) {
-            pop.removeChild(pop.firstChild)
-        }
     }
-
-    // container.classList.toggle = "active"
 }
 container.addEventListener("click", makePopup)
